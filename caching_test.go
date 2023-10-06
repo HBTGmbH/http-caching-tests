81f7dab9be1e803df7272a311133e666f8e52c49
Kai Burjack <kai.burjack@hbt.de>
2023-10-06 12:36:22 +0200

    Add two more tests for handling of 500 responses

--- caching_test.go
+++ caching_test.go
@@ -6,6 +6,8 @@ import (
 	"github.com/stretchr/testify/require"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
+	"sync"
 	"testing"
 	"time"
 )
@@ -36,22 +38,113 @@ func TestNoCacheControl(t *testing.T) {
 	waitForHealthy(t, port)
 
 	// send request
-	assert.Equal(t, "foo", req(t, port, "foo"))
+	assert.Equal(t, "foo", reqR(t, port, "foo").xResponse)
 
 	// wait half a second
 	time.Sleep(500 * time.Millisecond)
 
 	// send another request and expect the previous cached return
-	assert.Equal(t, "foo", req(t, port, "bar"))
+	assert.Equal(t, "foo", reqR(t, port, "bar").xResponse)
 
 	// wait for 600 ms
 	time.Sleep(600 * time.Millisecond)
 
 	// send another request and expect no cached return
-	assert.Equal(t, "baz", req(t, port, "baz"))
+	assert.Equal(t, "baz", reqR(t, port, "baz").xResponse)
 
 	// expect two backend requests
 	assert.Equal(t, 2, backendRequests)
+}
+
+// TestNoCachingOf500ErrorOnFirstRequest tests that Varnish will not cache an initial 500 error
+// response from the backend when Varnish did not yet have a non 5xx response in its cache.
+// The scenario here is: Varnish starts up and the backend responds with 500. In that case, Varnish
+// will not start to cache these 500 errors, but request each time from the backend.
+func TestNoCachingOf500ErrorOnFirstRequest(t *testing.T) {
+	t.Parallel()
+	var backendRequests int
+
+	// start a test server
+	testServerPort, testServer := startTestServer(func(w http.ResponseWriter, r *http.Request) {
+		xStatusCode, err := strconv.Atoi(r.Header.Get("X-Status-Code"))
+		w.Header().Set("X-Response", r.Header.Get("X-Request"))
+		assert.NoError(t, err)
+		w.WriteHeader(xStatusCode)
+		backendRequests++
+	})
+	defer testServer.Close()
+
+	// start varnish container
+	port, stopFunc, err := caching.StartVarnishInDocker(caching.VarnishConfig{
+		BackendPort: testServerPort,
+		DefaultTtl:  "1s",
+	})
+	require.NoError(t, err)
+	defer stopFunc()
+	waitForHealthy(t, port)
+
+	// send request resulting in 500
+	assert.Equal(t, resp(http.StatusInternalServerError, "1"), reqSR(t, port, http.StatusInternalServerError, "1"))
+
+	// wait half a second
+	time.Sleep(500 * time.Millisecond)
+
+	// send another request and expect the previous cached error
+	assert.Equal(t, resp(http.StatusOK, "2"), reqSR(t, port, http.StatusOK, "2"))
+
+	// expect two backend requests (because the first one wasn't cached)
+	assert.Equal(t, 2, backendRequests)
+}
+
+func TestNoCachingOf500ErrorInGracePeriodAfter200Request(t *testing.T) {
+	t.Parallel()
+	var backendRequests int
+
+	// start a test server
+	testServerPort, testServer := startTestServer(func(w http.ResponseWriter, r *http.Request) {
+		xStatusCode, err := strconv.Atoi(r.Header.Get("X-Status-Code"))
+		w.Header().Set("X-Response", r.Header.Get("X-Request"))
+		assert.NoError(t, err)
+		w.WriteHeader(xStatusCode)
+		backendRequests++
+	})
+	defer testServer.Close()
+
+	// start varnish container
+	port, stopFunc, err := caching.StartVarnishInDocker(caching.VarnishConfig{
+		BackendPort:  testServerPort,
+		DefaultTtl:   "1s",
+		DefaultGrace: "5s",
+	})
+	require.NoError(t, err)
+	defer stopFunc()
+	waitForHealthy(t, port)
+
+	// send request resulting in 200
+	assert.Equal(t, resp(http.StatusOK, "1"), reqSR(t, port, http.StatusOK, "1"))
+
+	// wait 1.1 seconds to let the response expire
+	time.Sleep(1100 * time.Millisecond)
+
+	// send another request which would result in 500 but still expect the previous cached 200 response
+	// because we are still in the grace period and within that Varnish will perform background revalidation
+	// asynchronous to the client's request.
+	assert.Equal(t, resp(http.StatusOK, "1"), reqSR(t, port, http.StatusInternalServerError, "2"))
+
+	// wait a bit for Varnish to revalidate the cached response. After this, Varnish will have
+	// abandoned the cached 200 response and will also not have cached the 500 response resulting
+	// in subsequent requests to always hit the backend.
+	time.Sleep(100 * time.Millisecond)
+
+	// send another request which will result in a backend fetch returning 500.
+	assert.Equal(t, resp(http.StatusInternalServerError, "3"), reqSR(t, port, http.StatusInternalServerError, "3"))
+
+	// send yet another request which will also result in a backend fetch returning 500
+	// indicating that the previous response has not been cached.
+	assert.Equal(t, resp(http.StatusInternalServerError, "4"), reqSR(t, port, http.StatusInternalServerError, "4"))
+
+	// expect three backend requests
+	assert.Equal(t, 4, backendRequests)
 }
 
 // TestCacheControlNoCache tests that Varnish does not respond with a cached item
@@ -85,10 +178,10 @@ func TestCacheControlNoCache(t *testing.T) {
 	waitForHealthy(t, port)
 
 	// send request
-	assert.Equal(t, "foo", req(t, port, "foo"))
+	assert.Equal(t, "foo", reqR(t, port, "foo").xResponse)
 
 	// send another request
-	assert.Equal(t, "bar", req(t, port, "bar"))
+	assert.Equal(t, "bar", reqR(t, port, "bar").xResponse)
 
 	// expect two backend requests
 	assert.Equal(t, 2, backendRequests)
@@ -118,10 +211,10 @@ func TestCacheControlMaxAge1(t *testing.T) {
 	waitForHealthy(t, port)
 
 	// send request to varnish
-	assert.Equal(t, "1", req(t, port, "1"))
+	assert.Equal(t, "1", reqR(t, port, "1").xResponse)
 
 	// send another request and expect to receive a cached response
-	assert.Equal(t, "1", req(t, port, "2"))
+	assert.Equal(t, "1", reqR(t, port, "2").xResponse)
 
 	// expect one backend request
 	assert.Equal(t, 1, backendRequests)
@@ -156,14 +249,14 @@ func TestStaleWhileRevalidate(t *testing.T) {
 	waitForHealthy(t, port)
 
 	// send request to varnish
-	assert.Equal(t, "1", req(t, port, "1"))
+	assert.Equal(t, "1", reqR(t, port, "1").xResponse)
 
 	// sleep for 1.1 seconds to make the cached response stale
 	time.Sleep(1100 * time.Millisecond)
 
 	// send another request and expect to receive a cached response
 	time1 := time.Now()
-	assert.Equal(t, "1", req(t, port, "2"))
+	assert.Equal(t, "1", reqR(t, port, "2").xResponse)
 	time2 := time.Now()
 	// expect the response to have come back very fast
 	assert.Less(t, time2.Sub(time1), 100*time.Millisecond)
@@ -172,7 +265,7 @@ func TestStaleWhileRevalidate(t *testing.T) {
 	time.Sleep(2100 * time.Millisecond)
 
 	// send yet another request and expect to receive the second cached response
-	assert.Equal(t, "2", req(t, port, "3"))
+	assert.Equal(t, "2", reqR(t, port, "3").xResponse)
 
 	// expect two backend requests
 	assert.Equal(t, 2, backendRequests)
@@ -214,16 +307,21 @@ func TestHitForMissAndNoRequestCoalescingWhenNoStore(t *testing.T) {
 	const N = 10
 
 	// send N requests in parallel
-	ch := make(chan string, N)
+	var wg sync.WaitGroup
+	wg.Add(N)
 	for i := 0; i < N; i++ {
-		go func() { ch <- req(t, port, "1") }()
+		var i = i
+		go func() {
+			// and assert that each request (with each individual X-Request header)
+			// gets a response with its corresponding individual X-Response header
+			assert.Equal(t, strconv.Itoa(i), reqR(t, port, strconv.Itoa(i)).xResponse)
+			wg.Done()
+		}()
 	}
 
 	// expect N responses, but NOT all of them serialized!
 	time1 := time.Now()
-	for i := 0; i < N; i++ {
-		assert.Equal(t, "1", <-ch)
-	}
+	wg.Wait()
 	time2 := time.Now()
 
 	// expect all but the first response to have come back in parallel.
@@ -232,7 +330,8 @@ func TestHitForMissAndNoRequestCoalescingWhenNoStore(t *testing.T) {
 	// the following N-1 requests in parallel, which will all take sleepTime
 	// together to respond.
 	// Therefore, the whole test case is completed after about 2 * sleepTime.
-	assert.Less(t, time2.Sub(time1), sleepTime+sleepTime+100*time.Millisecond)
+	assert.Less(t, time2.Sub(time1), 2*sleepTime+100*time.Millisecond)
+	assert.Greater(t, time2.Sub(time1), 2*sleepTime-100*time.Millisecond)
 
 	// expect N backend requests
 	assert.Equal(t, N, backendRequests)
@@ -240,15 +339,34 @@ func TestHitForMissAndNoRequestCoalescingWhenNoStore(t *testing.T) {
 
 // ################ Test fixtures and helpers ################
 
-func req(t *testing.T, port, xRequestHeader string) string {
+type response struct {
+	statusCode int
+	xResponse  string
+}
+
+func resp(statusCode int, xResponse string) response {
+	return response{
+		statusCode: statusCode,
+		xResponse:  xResponse,
+	}
+}
+
+func reqR(t *testing.T, port string, xRequest string) response {
+	return reqSR(t, port, http.StatusOK, xRequest)
+}
+
+func reqSR(t *testing.T, port string, status int, xRequest string) response {
 	httpClient := http.Client{}
 	req, err := http.NewRequest("GET", "http://localhost:"+port+"/", nil)
-	req.Header.Set("X-Request", xRequestHeader)
+	req.Header.Set("X-Status-Code", strconv.Itoa(status))
+	req.Header.Set("X-Request", xRequest)
 	assert.NoError(t, err)
 	resp, err := httpClient.Do(req)
 	assert.NoError(t, err)
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
-	return resp.Header.Get("X-Response")
+	return response{
+		statusCode: resp.StatusCode,
+		xResponse:  resp.Header.Get("X-Response"),
+	}
 }
 
 func startTestServer(handler http.HandlerFunc) (string, *httptest.Server) {
