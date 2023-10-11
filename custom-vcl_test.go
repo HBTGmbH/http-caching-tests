f8fcc2484c8835f8a006f5789b1194fa3f56df71
Kai Burjack <kai.burjack@hbt.de>
2023-10-11 13:50:46 +0200

    Add tests for Hit-for-Miss expectations for error responses

--- custom-vcl_test.go
+++ custom-vcl_test.go
@@ -84,3 +84,91 @@ sub vcl_backend_response {
 	// 3. next background request with 500 response (which was also abandoned)
 	assert.Equal(t, 3, backendRequests)
 }
+
+// TestTtlOn5xxBackendResponseIsNotAutomaticallyHitForMiss will implement and test the idea of
+// https://info.varnish-software.com/blog/hit-for-miss-and-why-a-null-ttl-is-bad-for-you
+// and check that setting a TTL and grace for 5xx responses will not automatically mark
+// beresp.uncacheable as true, in which case the response becomes a Hit-For-Miss object.
+func TestTtlOn5xxBackendResponseIsNotAutomaticallyHitForMiss(t *testing.T) {
+	t.Parallel()
+	var backendRequests int
+
+	// start a test server
+	testServerPort, testServer := startTestServer(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("X-Response", r.Header.Get("X-Request"))
+		w.WriteHeader(http.StatusInternalServerError)
+		backendRequests++
+	})
+	defer testServer.Close()
+
+	// start varnish container with a custom VCL
+	port, stopFunc, err := caching.StartVarnishInDocker(caching.VarnishConfig{
+		BackendPort: testServerPort,
+		Vcl: `
+sub vcl_backend_response {
+  if (beresp.status == 500 || (beresp.status >= 502 && beresp.status <= 504)) {
+    set beresp.ttl = 1s;
+    set beresp.grace = 10s;
+  }
+}`,
+	})
+	require.NoError(t, err)
+	defer stopFunc()
+	waitForHealthy(t, port)
+
+	// send request which will become a 500 response
+	assert.Equal(t, resp(http.StatusInternalServerError, "foo"), reqR(t, port, "foo"))
+
+	// wait a tiny bit
+	time.Sleep(100 * time.Millisecond)
+
+	// send another request and expect the previous cached return
+	assert.Equal(t, resp(http.StatusInternalServerError, "foo"), reqR(t, port, "bar"))
+
+	// expect one backend request
+	assert.Equal(t, 1, backendRequests)
+}
+
+// TestExplicitMarkingAsUncacheableOn5xxBackendResponseIsHitForMiss checks that when we _do_
+// explicitly set beresp.uncacheable to true, we get a Hit-For-Miss object.
+// This is somewhat the inverse of the TestTtlOn5xxBackendResponseIsNotAutomaticallyHitForMiss above.
+func TestExplicitMarkingAsUncacheableOn5xxBackendResponseIsHitForMiss(t *testing.T) {
+	t.Parallel()
+	var backendRequests int
+
+	// start a test server
+	testServerPort, testServer := startTestServer(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("X-Response", r.Header.Get("X-Request"))
+		w.WriteHeader(http.StatusInternalServerError)
+		backendRequests++
+	})
+	defer testServer.Close()
+
+	// start varnish container with a custom VCL
+	port, stopFunc, err := caching.StartVarnishInDocker(caching.VarnishConfig{
+		BackendPort: testServerPort,
+		Vcl: `
+sub vcl_backend_response {
+  if (beresp.status == 500 || (beresp.status >= 502 && beresp.status <= 504)) {
+    set beresp.ttl = 1s;
+    set beresp.grace = 10s;
+    set beresp.uncacheable = true;
+  }
+}`,
+	})
+	require.NoError(t, err)
+	defer stopFunc()
+	waitForHealthy(t, port)
+
+	// send request which will become a 500 response
+	assert.Equal(t, resp(http.StatusInternalServerError, "foo"), reqR(t, port, "foo"))
+
+	// wait a tiny bit
+	time.Sleep(100 * time.Millisecond)
+
+	// send another request and expect a new backend request because of Hit-For-Miss
+	assert.Equal(t, resp(http.StatusInternalServerError, "bar"), reqR(t, port, "bar"))
+
+	// expect two backend requests
+	assert.Equal(t, 2, backendRequests)
+}
